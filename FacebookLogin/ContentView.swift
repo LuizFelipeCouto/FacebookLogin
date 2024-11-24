0a8faf53e925dd72e30e3f47a82ccb0d61f35eb9
lfelipe.dev <129457138+LuizFelipeCouto@users.noreply.github.com>
2024-11-24 19:46:16 -0300

    Initial Commit

---
+++ FacebookLogin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FacebookLogin
+//
+//  Created by Luiz Felipe Aparecido do Couto on 24/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FacebookLogin/FacebookLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacebookLoginApp.swift
+//  FacebookLogin
+//
+//  Created by Luiz Felipe Aparecido do Couto on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FacebookLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
