c55d820c62d4a2c6337a07184d45c8437405413f
lfelipe.dev <129457138+LuizFelipeCouto@users.noreply.github.com>
2024-11-26 05:34:45 -0300

    Foi desenvolvida toda a tela de login

--- FacebookLogin/ContentView.swift
+++ FacebookLogin/ContentView.swift
@@ -6,19 +6,70 @@
 //
 
 import SwiftUI
+struct ContentView: View{
+    
+    @State var email: String = ""
+    @State var password: String = ""
+    
+    var body: some View{
+        ZStack{
+            Color("backgroundColor").ignoresSafeArea()
+            
+            VStack{
+                Image("Facebook")
+                
+                Spacer().frame(height: 64)
+                
+                VStack {
+                    TextField("Email", text: $email)
+                        .padding()
+                    
+                    Divider()
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                }.background(Color.white)
+                    .cornerRadius(4.0)
+                    .padding()
+                
+                Button("Login") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }.font(.title3)
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(20)
+                    .frame(maxWidth: .infinity)
+                    .background(Color("ButtonColor"))
+                    .cornerRadius(4.0)
+                    .padding()
+                    .padding(.top, -20)
+                
+                Spacer().frame(height: 126)
+                
+                
+                 Button("Dont't have an account? Sign up") {
+                     /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                 }.foregroundColor(.white)
+                    .bold()
+                
+                Spacer().frame(height: 8)
+                
+                Button("Forgot you password?") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }.foregroundColor(Color("TextColor"))
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+                
+            }
+            
+            
         }
-        .padding()
+                
+        }
+        
     }
-}
-
-#Preview {
+    
+struct contentView_Previews: PreviewProvider {
+    static var previews: some View {
     ContentView()
+    }
 }
